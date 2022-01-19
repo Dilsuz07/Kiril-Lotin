d2b4613a304e412e841c3f78e8bd344aa93ebb4b
Dilsuz07 <dilsuz25012303@gmail.com>
2022-01-19 13:54:07 +0500

    no commit

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ var kiril=["ю","Ю","я","Я","ё","Ё","ш","Ш","ч","Ч","ў","Ў",
     "э","Э","ж","Ж","д","Д","л","Л","о","О","р","Р",
     "п","П","а","А","в","В","ф","Ф","с","С","м","М",
     "и","И","т","Т","б","Б","қ","Қ","ҳ","Ҳ","ғ","Ғ","ь"];
-     
-//lotin harflari massivi    
+       
 var lot=["yu","Yu","ya","Ya","yo","Yo","sh","Sh","ch","Ch","o'","O'",
           "q","Q","g'","G'","ts","Ts","y","Y","u","U","k","K","e","E",
     "n","N","g","G","sh","Sh","z","Z","x","X","e","E","j","J",
@@ -13,25 +12,25 @@ var lot=["yu","Yu","ya","Ya","yo","Yo","sh","Sh","ch","Ch","o'","O'",
     "F","s","S","m","M","i","I","t","T","b","B","q","Q","h","H",
     "g'","G'","`"];
      
-//kirildan lotinga o'tkazish funksiyasi  
+ 
 var tugma_1 = document.querySelector('#btn-1')
 var tugma_2 = document.querySelector('#btn-2')
 var tugma_3 = document.querySelector('#btn-3')
+
 function kirillot(){
 var i;
 var satr = document.getElementById("matn").value;
    for (i = 0; i < kiril.length; i++) {
- //xar bir harfni alohida almashtiramiz
+ 
    satr=satr.replaceAll(kiril[i],lot[i]);
 }
    document.getElementById("matn").value = satr;  
 }
 
-//lotindan kirilga o'tkazish funksiyasi
 function lotkiril(){
 var satr = document.getElementById("matn").value;
    for (i = 0; i < kiril.length; i++) {
- //xar bir harfni alohida almashtiramiz
+
    satr=satr.replaceAll(lot[i],kiril[i]);
 }
    document.getElementById("matn").value = satr;  
@@ -65,7 +64,6 @@ tugma_3.addEventListener('mouseleave',function(){
 
 })
 
-//Matn maydonini tozalash funksiyasi
 function tozalash(){
 document.getElementById("matn").value = "";  
 }
